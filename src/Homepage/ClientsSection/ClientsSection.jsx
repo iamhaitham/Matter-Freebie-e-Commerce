2f941efa72a0bae0a2fd98f707fac2b00760dce6
iamhaitham <iamhaitham2011@gmail.com>
2022-10-05 20:01:10 +0300

    Create ClientsSection component

---
+++ src/Homepage/ClientsSection/ClientsSection.jsx
@@ -0,0 +1,16 @@
+import SectionTitle from '../../shared/SectionTitle/SectionTitle';
+import Section from '../Section/Sections';
+import Image from '../../shared/Image/Image';
+
+function ClientsSection() {
+    const content = `As Seen On`;
+
+    return (
+      <Section>
+        <SectionTitle content={ content }/>
+        <Image src='/images/as-seen-on.png' alt='clients'/>
+      </Section>
+    );
+}
+
+export default ClientsSection;
