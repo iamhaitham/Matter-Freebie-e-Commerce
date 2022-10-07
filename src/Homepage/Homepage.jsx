bff7c7def337eceeed89d0a52fde58b4e524fbdc
iamhaitham <iamhaitham2011@gmail.com>
2022-10-07 16:15:07 +0300

    Add 'ExploreSection' component in 'Homepage' component

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import ClientsSection from "./ClientsSection/ClientsSection";
+import ExploreSection from "./ExploreSection/ExploreSection";
 import RecommendedVideosSection from "./RecommendedVideosSection/RecommendedVideosSection";
 import ShopSection from "./ShopSection/ShopSection";
 
 function Homepage() {
     return (
         <>
+            <ExploreSection/>
             <ShopSection/>
             <RecommendedVideosSection/>
             <ClientsSection/>
