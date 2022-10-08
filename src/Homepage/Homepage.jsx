c7b685d78a9e3c4ad39a23f105c98b9c676cde9e
iamhaitham <iamhaitham2011@gmail.com>
2022-10-08 13:40:43 +0300

    Add 'AboutSection' component to 'Homepage'

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -2,10 +2,12 @@ import ClientsSection from "./ClientsSection/ClientsSection";
 import ExploreSection from "./ExploreSection/ExploreSection";
 import RecommendedVideosSection from "./RecommendedVideosSection/RecommendedVideosSection";
 import ShopSection from "./ShopSection/ShopSection";
+import AboutSection from './AboutSection/AboutSection';
 
 function Homepage() {
     return (
         <>
+            <AboutSection/>
             <ExploreSection/>
             <ShopSection/>
             <RecommendedVideosSection/>
