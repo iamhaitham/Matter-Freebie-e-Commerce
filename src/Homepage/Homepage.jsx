9cd67af9d7adc0614d0b0bfd208c058e53e87c19
iamhaitham <iamhaitham2011@gmail.com>
2022-10-14 15:34:04 +0300

    Add 'CarouselSection' to 'Homepage'

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -1,13 +1,15 @@
-import ClientsSection from "./ClientsSection/ClientsSection";
-import ExploreSection from "./ExploreSection/ExploreSection";
-import RecommendedVideosSection from "./RecommendedVideosSection/RecommendedVideosSection";
-import ShopSection from "./ShopSection/ShopSection";
+import ClientsSection from './ClientsSection/ClientsSection';
+import ExploreSection from './ExploreSection/ExploreSection';
+import RecommendedVideosSection from './RecommendedVideosSection/RecommendedVideosSection';
+import ShopSection from './ShopSection/ShopSection';
 import AboutSection from './AboutSection/AboutSection';
-import FeaturedSection from "./FeaturedSection/FeaturedSection";
+import FeaturedSection from './FeaturedSection/FeaturedSection';
+import CarouselSection from './CarouselSection/CarouselSection';
 
 function Homepage() {
     return (
         <>
+            <CarouselSection/>
             <FeaturedSection/>
             <AboutSection/>
             <ExploreSection/>
