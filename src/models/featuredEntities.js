b39f73566353926fdea6b3a439d65b4dfaa5e98a
iamhaitham <iamhaitham2011@gmail.com>
2022-11-07 20:08:57 +0200

    Extract the featuredEntities to a separate file, and change the references

---
+++ src/models/featuredEntities.js
@@ -0,0 +1,245 @@
+const featuredEntities = [
+    { 
+        id: 1, 
+        isNew: true, 
+        description: 'Pueraria Mirifica And Study Phyto Estrogens 1', 
+        src: '/images/featured-7.png', 
+        alt: 'featured-1', 
+        price: '599.00', 
+        miniImages: [
+            { 
+                id: 1, 
+                src: '/images/minis-1.png', 
+                alt: 'Mini 1' 
+            }, { 
+                id: 2,
+                src: '/images/minis-2.png', 
+                alt: 'Mini 2' 
+            }, { 
+                id: 3, 
+                src: '/images/minis-3.png', 
+                alt: 'Mini 3' 
+            }, { 
+                id: 4, 
+                src: '/images/minis-4.png', 
+                alt: 'Mini 4' 
+            }, { 
+                id: 5, 
+                src: '/images/minis-2.png',
+                alt: 'Mini 5' 
+            }
+        ]
+    }, { 
+        id: 2, 
+        isNew: true, 
+        description: 'Pueraria Mirifica And Study Phyto Estrogens 2', 
+        src: '/images/featured-2.png', 
+        alt: 'featured-2', 
+        price: '599.00', 
+        miniImages: [
+            { 
+                id: 1, 
+                src: '/images/minis-1.png', 
+                alt: 'Mini 1' 
+            }, { 
+                id: 2,
+                src: '/images/minis-2.png', 
+                alt: 'Mini 2' 
+            }, { 
+                id: 3, 
+                src: '/images/minis-3.png', 
+                alt: 'Mini 3' 
+            }, { 
+                id: 4, 
+                src: '/images/minis-4.png', 
+                alt: 'Mini 4' 
+            }, { 
+                id: 5, 
+                src: '/images/minis-2.png',
+                alt: 'Mini 5' 
+            }
+        ]
+    }, { 
+        id: 3, 
+        isNew: true, 
+        description: 'Pueraria Mirifica And Study Phyto Estrogens 3', 
+        src: '/images/featured-8.png', 
+        alt: 'featured-3', 
+        price: '599.00', 
+        miniImages: [
+            { 
+                id: 1, 
+                src: '/images/minis-1.png', 
+                alt: 'Mini 1' 
+            }, { 
+                id: 2,
+                src: '/images/minis-2.png', 
+                alt: 'Mini 2' 
+            }, { 
+                id: 3, 
+                src: '/images/minis-3.png', 
+                alt: 'Mini 3' 
+            }, { 
+                id: 4, 
+                src: '/images/minis-4.png', 
+                alt: 'Mini 4' 
+            }, { 
+                id: 5, 
+                src: '/images/minis-2.png',
+                alt: 'Mini 5' 
+            }
+        ] 
+    }, { 
+        id: 4, 
+        isNew: false, 
+        description: 'Pueraria Mirifica And Study Phyto Estrogens 4', 
+        src: '/images/featured-4.png', 
+        alt: 'featured-4', 
+        price: '599.00', 
+        miniImages: [
+            { 
+                id: 1, 
+                src: '/images/minis-1.png', 
+                alt: 'Mini 1' 
+            }, { 
+                id: 2,
+                src: '/images/minis-2.png', 
+                alt: 'Mini 2' 
+            }, { 
+                id: 3, 
+                src: '/images/minis-3.png', 
+                alt: 'Mini 3' 
+            }, { 
+                id: 4, 
+                src: '/images/minis-4.png', 
+                alt: 'Mini 4' 
+            }, { 
+                id: 5, 
+                src: '/images/minis-2.png',
+                alt: 'Mini 5' 
+            }
+        ] 
+    }, { 
+        id: 5, 
+        isNew: true, 
+        description: 'Pueraria Mirifica And Study Phyto Estrogens 5', 
+        src: '/images/featured-5.png', 
+        alt: 'featured-5', 
+        price: '599.00', 
+        miniImages: [
+            { 
+                id: 1, 
+                src: '/images/minis-1.png', 
+                alt: 'Mini 1' 
+            }, { 
+                id: 2,
+                src: '/images/minis-2.png', 
+                alt: 'Mini 2' 
+            }, { 
+                id: 3, 
+                src: '/images/minis-3.png', 
+                alt: 'Mini 3' 
+            }, { 
+                id: 4, 
+                src: '/images/minis-4.png', 
+                alt: 'Mini 4' 
+            }, { 
+                id: 5, 
+                src: '/images/minis-2.png',
+                alt: 'Mini 5' 
+            }
+        ] 
+    }, { 
+        id: 6, 
+        isNew: false, 
+        description: 'Pueraria Mirifica And Study Phyto Estrogens 6', 
+        src: '/images/featured-6.png', 
+        alt: 'featured-6', 
+        price: '599.00', 
+        miniImages: [
+            { 
+                id: 1, 
+                src: '/images/minis-1.png', 
+                alt: 'Mini 1' 
+            }, { 
+                id: 2,
+                src: '/images/minis-2.png', 
+                alt: 'Mini 2' 
+            }, { 
+                id: 3, 
+                src: '/images/minis-3.png', 
+                alt: 'Mini 3' 
+            }, { 
+                id: 4, 
+                src: '/images/minis-4.png', 
+                alt: 'Mini 4' 
+            }, { 
+                id: 5, 
+                src: '/images/minis-2.png',
+                alt: 'Mini 5' 
+            }
+        ] 
+    }, { 
+        id: 7, 
+        isNew: true, 
+        description: 'Pueraria Mirifica And Study Phyto Estrogens 7', 
+        src: '/images/featured-7.png', 
+        alt: 'featured-7', 
+        price: '599.00', 
+        miniImages: [
+            { 
+                id: 1, 
+                src: '/images/minis-1.png', 
+                alt: 'Mini 1' 
+            }, { 
+                id: 2,
+                src: '/images/minis-2.png', 
+                alt: 'Mini 2' 
+            }, { 
+                id: 3, 
+                src: '/images/minis-3.png', 
+                alt: 'Mini 3' 
+            }, { 
+                id: 4, 
+                src: '/images/minis-4.png', 
+                alt: 'Mini 4' 
+            }, { 
+                id: 5, 
+                src: '/images/minis-2.png',
+                alt: 'Mini 5' 
+            }
+        ] 
+    }, { 
+        id: 8, 
+        isNew: false, 
+        description: 'Pueraria Mirifica And Study Phyto Estrogens 8', 
+        src: '/images/featured-8.png', 
+        alt: 'featured-8', 
+        price: '599.00', 
+        miniImages: [
+            { 
+                id: 1, 
+                src: '/images/minis-1.png', 
+                alt: 'Mini 1' 
+            }, { 
+                id: 2,
+                src: '/images/minis-2.png', 
+                alt: 'Mini 2' 
+            }, { 
+                id: 3, 
+                src: '/images/minis-3.png', 
+                alt: 'Mini 3' 
+            }, { 
+                id: 4, 
+                src: '/images/minis-4.png', 
+                alt: 'Mini 4' 
+            }, { 
+                id: 5, 
+                src: '/images/minis-2.png',
+                alt: 'Mini 5' 
+            }
+        ] 
+    }
+];
+
+export default featuredEntities;
