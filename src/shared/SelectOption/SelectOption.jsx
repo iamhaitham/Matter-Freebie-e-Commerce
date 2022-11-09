28c83e144627f6145ba89967af8423e17f0e5100
iamhaitham <iamhaitham2011@gmail.com>
2022-11-09 20:58:15 +0200

    Lift the state upward from SelectOption to ProductListingSortBySection

--- src/shared/SelectOption/SelectOption.jsx
+++ src/shared/SelectOption/SelectOption.jsx
@@ -1,11 +1,12 @@
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import { useState } from 'react';
 
-function SelectOption({ optionsEntities }) {
-    const [selectedOption, setSelectedOption] =  useState(optionsEntities[0].optionName);
-
+function SelectOption({ 
+    optionsEntities, 
+    selectedOption, 
+    handleSelectionChange 
+}) {
     const options = optionsEntities.map(entity => {
         const { id, optionName } = entity;
 
@@ -13,10 +14,6 @@ function SelectOption({ optionsEntities }) {
             <MenuItem key={ id } value={ optionName }>{ optionName }</MenuItem>
         );
     });
-
-    const handleSelectionChange = (e) => {
-        setSelectedOption(e.target.value);
-    }
 
     return (
         <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
