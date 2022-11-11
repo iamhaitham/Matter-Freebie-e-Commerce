b74cdbd80a4170f7c4192fd2921c15649da12eae
iamhaitham <iamhaitham2011@gmail.com>
2022-11-11 16:00:37 +0200

    Create filtersEntities

---
+++ src/models/filtersEntities.js
@@ -0,0 +1,19 @@
+const filtersEntities = [
+    { id: 1, name: 'Apparels', subFilters: [
+        { id: 1, name: 'PANTS' },
+        { id: 1, name: 'JUMPSUITS' },
+        { id: 1, name: 'SHORTS' },
+        { id: 1, name: 'TOPS' }]
+    },
+    { id: 2, name: 'Accessories' },
+    { id: 3, name: 'Houseware' },
+    { id: 4, name: 'Others' },
+    { id: 5, name: 'Techniques' },
+    { id: 6, name: 'Styles', subFilters: [
+        { id: 1, name: 'BOLD' },
+        { id: 1, name: 'MONOCHROME' },
+        { id: 1, name: 'NEUTRALS' }]
+    }
+];
+
+export default filtersEntities;
