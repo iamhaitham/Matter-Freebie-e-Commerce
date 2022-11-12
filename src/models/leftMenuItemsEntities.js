98cb3d1c9eac10fe602d2ac3e14003f0e39bc808
iamhaitham <iamhaitham2011@gmail.com>
2022-11-12 13:20:53 +0200

    Extract leftMenuItemsEntities to a separate model

---
+++ src/models/leftMenuItemsEntities.js
@@ -0,0 +1,8 @@
+const leftMenuItemsEntities = [
+    { id: 1, name: 'SHOP', isArrowIncluded: true },
+    { id: 2, name: 'FABRIC', isArrowIncluded: true },
+    { id: 3, name: 'JOURNAL', isArrowIncluded: true },
+    { id: 4, name: 'ABOUT', isArrowIncluded: true }
+];
+
+export default leftMenuItemsEntities;
