886b8762d70d67b4b035182ef197c5a66249418b
iamhaitham <iamhaitham2011@gmail.com>
2022-11-20 21:59:02 +0200

    Show a badge with the number of unique items in the cart

--- src/shared/NavigationMenu/RightNavigationMenu/RightNavigationMenuIcons/RightNavigationMenuIcons.jsx
+++ src/shared/NavigationMenu/RightNavigationMenu/RightNavigationMenuIcons/RightNavigationMenuIcons.jsx
@@ -1,20 +1,29 @@
+import { Badge } from '@mui/material';
 import Icon from '../../../Icon/Icon';
 import styles from './RightNavigationMenuIcons.module.css';
+import { iconsEntity } from '../../../../models';
+import useLocalStorageState from 'use-local-storage-state'
 
 function RightNavigationMenuIcons() {
+    const [cartItems] = useLocalStorageState('cartItems');
+
     const { iconStyles } = styles;
 
-    const iconsEntity = [
-        { id: 1, src: 'icons/search-icon.png', alt: 'Search Icon' },
-        { id: 2, src: 'icons/heart-icon.png', alt: 'Heart Icon' },
-        { id: 3, src: 'icons/shopping-bag-icon.png', alt: 'Shopping Bag Icon' }
-    ];
     const icons = iconsEntity.map(entity => {
         const { id, src, alt } = entity;
         
-        return (
-            <Icon key={ id } src={ src } alt={ alt } iconCss={ iconStyles }/>
-        );
+        if (id === 3 && cartItems && cartItems.length)
+            return (
+                <Badge key={ id } 
+                       badgeContent={ cartItems.length } 
+                       color="primary">
+                    <Icon src={ src } alt={ alt } iconCss={ iconStyles }/>
+                </Badge>
+            );
+        else 
+            return (
+                <Icon key={ id } src={ src } alt={ alt } iconCss={ iconStyles }/>
+            );
     });
     
     return (
