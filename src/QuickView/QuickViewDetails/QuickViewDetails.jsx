c67d0247266f1cab7e2414049f5221cf835d6569
iamhaitham <iamhaitham2011@gmail.com>
2022-11-20 20:10:22 +0200

    Extract detailsRowsEntities to a dedicated file

--- src/QuickView/QuickViewDetails/QuickViewDetails.jsx
+++ src/QuickView/QuickViewDetails/QuickViewDetails.jsx
@@ -6,6 +6,7 @@ import Button from '../../shared/Button/Button';
 import Counter from '../../shared/Counter/Counter';
 import StarContainer from '../../shared/Star/StarContainer/StarContainer';
 import sizesEntities from '../../models/sizesEntities';
+import detailsRowsEntities from '../../models/detailsRowsEntities';
 
 function QuickViewDetails({ title, price }) {
     const [selectedSize, setSelectedSize] = useState({});
@@ -21,13 +22,6 @@ function QuickViewDetails({ title, price }) {
             addToWatchListStyles, 
             addToCartStyles, 
         } = styles;
-
-    const detailsRowsEntities = [
-        { id: 1, rowTitle: 'Description', rowContent: 'Inspired by the continuous length of the lunghi or sarong seen in hot climates everywhere from South Asia to the Horn of Africa and southern Arabian pen' },
-        { id: 2, rowTitle: 'Artisan Employment', rowContent: '54 jobs' },
-        { id: 3, rowTitle: 'Partnership', rowContent: 'Randall Armstrong' },
-        { id: 4, rowTitle: 'In Collab', rowContent: 'Augusta Mendoza' }
-    ];
 
     function getRowStyles(id) {
         return id === 1
---
+++ src/models/detailsRowsEntities.js
@@ -0,0 +1,8 @@
+const detailsRowsEntities = [
+        { id: 1, rowTitle: 'Description', rowContent: 'Inspired by the continuous length of the lunghi or sarong seen in hot climates everywhere from South Asia to the Horn of Africa and southern Arabian pen' },
+        { id: 2, rowTitle: 'Artisan Employment', rowContent: '54 jobs' },
+        { id: 3, rowTitle: 'Partnership', rowContent: 'Randall Armstrong' },
+        { id: 4, rowTitle: 'In Collab', rowContent: 'Augusta Mendoza' }
+    ];
+
+export default detailsRowsEntities;
