dc288a21e5a457e518d3fd4e203211330e183f9f
iamhaitham <iamhaitham2011@gmail.com>
2022-11-23 00:03:28 +0200

    Create 'ToastNotification' component

---
+++ src/shared/ToastNotification/ToastNotification.jsx
@@ -0,0 +1,30 @@
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import { useState, forwardRef } from 'react';
+
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export default function ToastNotification({ errorMessage }) {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Stack spacing={2} sx={{ width: '100%' }}>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            { errorMessage }
+        </Alert>
+      </Snackbar>
+    </Stack>
+  );
+}
