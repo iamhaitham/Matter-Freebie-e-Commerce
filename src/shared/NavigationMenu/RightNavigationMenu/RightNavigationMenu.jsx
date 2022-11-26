e723edaa1622eaf78092ee88918dcb6443c93de7
iamhaitham <iamhaitham2011@gmail.com>
2022-11-26 20:33:07 +0200

    Handle the navigation to the login page when clicking on login

--- src/shared/NavigationMenu/RightNavigationMenu/RightNavigationMenu.jsx
+++ src/shared/NavigationMenu/RightNavigationMenu/RightNavigationMenu.jsx
@@ -1,16 +1,25 @@
+import { useNavigate } from 'react-router';
 import MenuItems from '../MenuItems/MenuItems';
 import RightNavigationMenuIcons from '../RightNavigationMenu/RightNavigationMenuIcons/RightNavigationMenuIcons';
 
 function RightNavigationMenu() {
     const rightNavigationMenuWrapper = `flex align-items-center`;
+    const navigate = useNavigate();
 
     const rightMenuItemsEntity = [
         { id: 1, name: 'LOGIN', isArrowIncluded: false }
     ];
 
+    const handleMenuItemClick = (_, id) => {
+        const item = rightMenuItemsEntity.find(entity => entity.id === id);
+        if (item.id === 1)
+            navigate(`${item.name.toLowerCase()}`);
+    }
+
     return (
         <div className={ rightNavigationMenuWrapper }>
-            <MenuItems entities={ rightMenuItemsEntity }/>
+            <MenuItems entities={ rightMenuItemsEntity }
+                       handleMenuItemClick={ handleMenuItemClick }/>
             <RightNavigationMenuIcons/>            
         </div>
     );
