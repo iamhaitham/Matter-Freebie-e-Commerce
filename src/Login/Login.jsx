cc3eb9746234a129bcaea2470882adfa82203b8b
iamhaitham <iamhaitham2011@gmail.com>
2022-11-26 18:50:21 +0200

    Create 'Login' component

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -10,86 +10,128 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { axiosLoginInstance } from '../services/custom';
+import ToastNotification from '../shared/ToastNotification/ToastNotification';
+import Loader from '../shared/Loader/Loader';
 
 const theme = createTheme();
 
-export default function SignIn() {
-    const [email, setEmail] = useState();
-    const emailRef = useRef();
-    
-    function isEmailValid() {
-        if(email && document.activeElement === emailRef.current)
-            return !email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+export default function Login() {
+    const [isLoader, setIsLoader] = useState(false);
+    const [email, setEmail] = useState('');
+    const [isEmailValid, setIsEmailValid] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+    const [errorMessage, setErrorMessage] = useState();
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+        setIsEmailValid(true);
     }
 
-    const handleSubmit = (event) => {
+    const handleEmailFieldLeave = (event) => {
+        setIsEmailValid(!!event.target.value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/))
+    }
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-    };
+
+        if (!isEmailValid) {
+            const message = 'Email is not valid!';
+            setErrorMessage(message)
+            setIsAuthenticated(false);
+            throw new Error(message);
+        }
+
+        const form = new FormData(event.currentTarget);
+        const password = form.get('password');
+
+        setIsLoader(true);
+
+        try {
+            await axiosLoginInstance.post('/login', {
+                username: email.split('@')[0],
+                password
+            });
+
+            setIsAuthenticated(true);
+        } catch (error) {          
+            if (error.response.status === 401) {
+                setErrorMessage('Incorrect email or password!')
+            } else {
+                setErrorMessage('Server Error!')
+            }
+
+            setIsAuthenticated(false);
+        }
+
+        setIsLoader(false);
+    }
+
+    const showLoader = () => {
+        if (isLoader)
+            return (
+                <Loader/>
+            );
+        else 
+            return (
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                        marginTop: 4,
+                        marginBottom: 6,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        }}>
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form" onSubmit={ (event) => handleSubmit(event) } noValidate sx={{ mt: 1 }}>
+                            <TextField
+                            onChange={ (event) => handleEmailChange(event) }
+                            onBlur={ (event) => handleEmailFieldLeave(event) }
+                            error={ !isEmailValid }
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus/>
+                            <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"/>
+                            <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"/>
+                            <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}>
+                            Sign In
+                            </Button>
+                        </Box>
+                    </Box>
+            </Container>
+            );
+    }
 
     return (
         <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                sx={{
-                    marginTop: 4,
-                    marginBottom: 6,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-                >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                    <TextField
-                    inputRef={ emailRef }
-                    onChange={ (e) => setEmail(e.target.value) }
-                    error={ isEmailValid() }
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    autoFocus
-                    />
-                    <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                    />
-                    <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                    >
-                    Sign In
-                    </Button>
-                </Box>
-                </Box>
-            </Container>
+            { showLoader() }
+            { isAuthenticated === false && <ToastNotification errorMessage={ errorMessage }/> }
         </ThemeProvider>
     );
 }
