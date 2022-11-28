a1ea46518746d8ee7d56778ce678c4d34d6360c2
iamhaitham <iamhaitham2011@gmail.com>
2022-11-28 13:56:40 +0200

    Only allow navigation to /login when user is not logged in yet

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Homepage from '../Homepage/Homepage';
 import Layout from './Layout';
 import ProductListing from '../ProductListing/ProductListing.jsx';
 import Login from '../Login/Login';
+import PrivateRoute from './PrivateRoute';
 
 function AppRoutes() {
     return (
@@ -11,7 +12,9 @@ function AppRoutes() {
                 <Route element={ <Layout/> }>
                     <Route index element={ <Homepage/> }/>
                     <Route path='shop' element={ <ProductListing/> }/>
-                    <Route path='login' element={ <Login/> }/>
+                    <Route element={ <PrivateRoute/> }>
+                        <Route path='login' element={ <Login/> }/>
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
