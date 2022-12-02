1c908bd3760d1c47439d06e2758148677e779cfe
iamhaitham <iamhaitham2011@gmail.com>
2022-12-02 15:04:50 +0200

    Move 'handleRightMenuItemClick' from 'RightNavigationMenu' to 'NavigationMenu'

--- src/shared/NavigationMenu/NavigationMenu.jsx
+++ src/shared/NavigationMenu/NavigationMenu.jsx
@@ -4,7 +4,8 @@ import LeftNavigationMenu from './LeftNavigationMenu/LeftNavigationMenu';
 import RightNavigationMenu from './RightNavigationMenu/RightNavigationMenu';
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import { leftMenuItemsEntities } from '../../entities';
+import { leftMenuItemsEntities, rightMenuItemsEntities } from '../../entities';
+import useLocalStorageState from 'use-local-storage-state';
 
 function NavigationMenu() {
     const { navigationMenu, routerLinkStyles } = styles;
@@ -15,11 +16,21 @@ function NavigationMenu() {
     const { navigationMenuWrapper, leftNavigationMenuWrapper } = navigationMenuCss;
 
     const [activeMenuItemId, setActiveMenuItemId] = useState();
+    const [userToken, , { removeItem }] = useLocalStorageState('userToken');
+    const rightMenuItems = userToken ? [rightMenuItemsEntities[1]] : [rightMenuItemsEntities[0]];
     const navigate = useNavigate();
 
-    const handleMenuItemClick = (e, id) => {
+    const handleLeftMenuItemClick = (_, id) => {
         const item = leftMenuItemsEntities.find(entity => entity.id === id);
         navigate(`${item.name.toLowerCase()}`);
+    }
+
+    const handleRightMenuItemClick = (_, id) => {
+        const item = rightMenuItemsEntities.find(entity => entity.id === id);
+        if (item.id === 1)
+            navigate(`${item.name.toLowerCase()}`);
+        else if (item.id === 2)
+            removeItem();
     }
 
     return (
@@ -33,10 +44,12 @@ function NavigationMenu() {
 
                 <LeftNavigationMenu activeMenuItemId={ activeMenuItemId }
                                     setActiveMenuItemId={ setActiveMenuItemId }
-                                    handleMenuItemClick={ handleMenuItemClick }
+                                    handleLeftMenuItemClick={ handleLeftMenuItemClick }
                                     leftMenuEntities={ leftMenuItemsEntities }/>
             </div>
-            <RightNavigationMenu/>
+
+            <RightNavigationMenu handleRightMenuItemClick={ handleRightMenuItemClick }
+                                 rightMenuEntities={ rightMenuItems }/>
         </div>
     );
 }
--- src/shared/NavigationMenu/RightNavigationMenu/RightNavigationMenu.jsx
+++ src/shared/NavigationMenu/RightNavigationMenu/RightNavigationMenu.jsx
@@ -1,27 +1,13 @@
-import { useNavigate } from 'react-router';
 import MenuItems from '../MenuItems/MenuItems';
 import RightNavigationMenuIcons from '../RightNavigationMenu/RightNavigationMenuIcons/RightNavigationMenuIcons';
-import { rightMenuItemsEntities } from '../../../entities';
-import useLocalStorageState from 'use-local-storage-state';
 
-function RightNavigationMenu() {
-    const rightNavigationMenuWrapper = `flex align-items-center`;
-    const navigate = useNavigate();
-    const [userToken, , { removeItem }] = useLocalStorageState('userToken');
-    const rightMenuItems = userToken ? [rightMenuItemsEntities[1]] : [rightMenuItemsEntities[0]];
-
-    const handleMenuItemClick = (_, id) => {
-        const item = rightMenuItemsEntities.find(entity => entity.id === id);
-        if (item.id === 1)
-            navigate(`${item.name.toLowerCase()}`);
-        else if (item.id === 2)
-            removeItem();
-    }
+function RightNavigationMenu({ rightMenuEntities, handleRightMenuItemClick }) {
+    const rightNavigationMenuWrapper = `flex align-items-center`;    
 
     return (
         <div className={ rightNavigationMenuWrapper }>
-            <MenuItems entities={ rightMenuItems }
-                       handleMenuItemClick={ handleMenuItemClick }/>
+            <MenuItems entities={ rightMenuEntities }
+                       handleMenuItemClick={ handleRightMenuItemClick }/>
             <RightNavigationMenuIcons/>            
         </div>
     );
