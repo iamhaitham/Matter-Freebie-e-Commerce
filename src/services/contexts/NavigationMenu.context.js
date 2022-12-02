5613983fcda51f18e52cf799da4bfc8965789b27
iamhaitham <iamhaitham2011@gmail.com>
2022-12-02 15:23:46 +0200

    Create NavigationMenuContext

---
+++ src/services/contexts/NavigationMenu.context.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+
+export const NavigationMenuContext = createContext(null);
--- src/services/contexts/index.js
+++ src/services/contexts/index.js
@@ -1,1 +1,2 @@
 export * from './FeaturedSection.context';
+export * from './NavigationMenu.context';
