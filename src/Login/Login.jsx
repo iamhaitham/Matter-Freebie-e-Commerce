70049cb170831dd8b244b9a726143a3eab1ebf3c
iamhaitham <iamhaitham2011@gmail.com>
2022-12-03 10:34:03 +0200

    Formatting adjustments

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -138,37 +138,36 @@ export default function Login() {
                         <Typography component="h1" variant="h5">
                             Sign in
                         </Typography>
-                        <Box component="form" onSubmit={ (event) => handleSubmit(event) } noValidate sx={{ mt: 1 }}>
-                            <TextField
-                            onChange={ (event) => handleEmailChange(event) }
-                            onBlur={ (event) => handleEmailFieldLeave(event) }
-                            error={ !state.isEmailValid }
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus/>
-                            <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"/>
-                            <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"/>
-                            <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}>
-                            Sign In
+                        <Box component="form" 
+                             onSubmit={ (event) => handleSubmit(event) } 
+                             noValidate 
+                             sx={{ mt: 1 }}>
+                            <TextField onChange={ (event) => handleEmailChange(event) }
+                                       onBlur={ (event) => handleEmailFieldLeave(event) }
+                                       error={ !state.isEmailValid }
+                                       margin="normal"
+                                       required
+                                       fullWidth
+                                       id="email"
+                                       label="Email Address"
+                                       name="email"
+                                       autoComplete="email"
+                                       autoFocus/>
+                            <TextField margin="normal"
+                                       required
+                                       fullWidth
+                                       name="password"
+                                       label="Password"
+                                       type="password"
+                                       id="password"
+                                       autoComplete="current-password"/>
+                            <FormControlLabel control={<Checkbox value="remember" color="primary" />}
+                                              label="Remember me"/>
+                            <Button type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}>
+                                    Sign In
                             </Button>
                         </Box>
                     </Box>
