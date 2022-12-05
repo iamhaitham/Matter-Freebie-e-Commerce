ae87132595bb6ed9c406234fda7f0a4087c95df2
iamhaitham <iamhaitham2011@gmail.com>
2022-12-05 23:16:35 +0200

    Create 'cartTableBodyEntities'

---
+++ src/entities/cartTableBodyEntities.js
@@ -0,0 +1,19 @@
+import { TableCell, TableRow } from "@mui/material";
+import { featuredEntities } from '.';
+
+const findItem = (itemId, field) => featuredEntities.filter(entity =>  entity.id === itemId).map(item => item[field]);
+
+export const cartTableBodyEntities = (cartItems) => {
+    return cartItems.map(item => {
+        const { itemId, quantity } = item;
+        
+        return (
+            <TableRow>
+                <TableCell align='center'>{ findItem(itemId, 'description') }</TableCell>
+                <TableCell align='center'>{ quantity }</TableCell>
+                <TableCell align='center'>${ findItem(itemId, 'price') * quantity }</TableCell>
+                <TableCell align='center'>xxxxxxxx</TableCell>
+            </TableRow>
+        );
+    })
+}
